f6ef30cb83fee14f7538fe54ccfea4547dca9a8b
Camila <camilabentespessoa@gmail.com.br>
2022-10-31 15:55:25 -0300

    funcao monitorando do beiçola

--- hello.go
+++ hello.go
@@ -2,52 +2,65 @@ package main
 
 import (
 	"fmt"
+	"net/http"
+	"os"
 )
 
 func main() {
-    fmt.Println("Hello, World!")
+    inicio()
 
-		nome := "Camila"
-		versao := 1.1
+    opcaoSelecionada := leitor()
 
-		fmt.Println("Olá, tia", nome)
-		fmt.Println("Aula GO em sua versão", versao)
+    switch opcaoSelecionada {
+    case 1:
+			inciarMonitoramento()
+    case 2:
+        fmt.Println("Log: Nunca use reduce")
+    case 0:
+        fmt.Println("You died, saia do jogo")
+				os.Exit(0)
+    default:
+        fmt.Println("Airam foi preso por não usar reduce")
+				os.Exit(-1)
+    }
 
-		fmt.Println("1 - Iniciar Monitoramento")
-		fmt.Println("2 - Exibir Logs")
-		fmt.Println("0 - Sair do Programa")
+}
 
-		var instrucao int 
-		fmt.Scan(&instrucao)
-		
-		fmt.Println("O valor da variável instrucao é:", instrucao)
+func inicio() {
+    nome := "Camila"
+    versao := 1.1
 
-		//expressão
-		// if instrucao == 1{
-		// 	fmt.Println("Estamos monitorando o Marcos")
-		// } else if instrucao == 2 {
-		// 	fmt.Println("Exibindo Logs do ZAP do Marcos")
-		// } else if instrucao == 3 {
-		// 	fmt.Println("Desisti e vou sair")
-		// } else {
-		// 	fmt.Println("Brasil vai melhorar - YOU DIED!")
-		// }
+    fmt.Println("Olá, tia", nome)
+    fmt.Println("Aula GO em sua versão", versao)
 
-		switch instrucao {
-		case 1:
-			fmt.Println("Estamos monitorando o Airam")
-		case 2:
-			fmt.Println("Log: Nunca use reduce")
-		case 0:
-			fmt.Println("You died, Airam é do inglês")
-		default:
-			fmt.Println("Airam foi preso por não usar reduce")
-			
-		}
+    fmt.Println("1 - Iniciar Monitoramento")
+    fmt.Println("2 - Exibir Logs")
+    fmt.Println("0 - Sair do Programa")
 
+}
 
+func leitor() int {
+
+    var instrucao int
+    fmt.Scan(&instrucao)
+
+    fmt.Println("O valor da variável instrucao é:", instrucao)
+    return instrucao
 }
 
 //1- Iniciar Monitoramento
 //2 - Exibir Logs
 //0 - Sair do Programa
+
+func inciarMonitoramento(){
+	fmt.Println("Estamos monitorando o Airam")
+	site := "https://sitecompras.vercel.app"
+	res, _ := http.Get(site) 
+	fmt.Println(res)
+
+	if res.StatusCode == 200{
+		fmt.Println("Site", site, "sucesso")
+	}else{
+		fmt.Println("Site", site, "ERRROU. Status Code:", res.StatusCode)
+	}
+}
